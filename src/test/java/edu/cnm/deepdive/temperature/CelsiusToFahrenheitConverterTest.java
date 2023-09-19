82ecf2f5a8921166dd4f8a3057f85d330b866dc0
Marc0E <mestradamonge@cnm.edu>
2023-09-19 10:42:52 -0600

    Added simple test.

--- src/test/java/edu/cnm/deepdive/temperature/CelsiusToFahrenheitConverterTest.java
+++ src/test/java/edu/cnm/deepdive/temperature/CelsiusToFahrenheitConverterTest.java
@@ -10,6 +10,7 @@ class CelsiusToFahrenheitConverterTest {
   void convert() {
     assertEquals(32, new CelsiusToFahrenheitConverter().convert(0),0.00001);
 
+
     assertEquals(212, new CelsiusToFahrenheitConverter().convert(100),0.00001);
 
     assertEquals(-40, new CelsiusToFahrenheitConverter().convert(-40),0.00001);
